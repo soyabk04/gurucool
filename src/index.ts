d1486e4e3347c47a49851bec5377074c2ee674b9
soyab <soyabsstudios@gmail.com>
2026-07-18 18:51:20 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,8 @@ const allowedOrigins = [
   "http://localhost:5173", // Vite
   "http://localhost:5500",
   "http://localhost:4173", // Vite Preview (optional)
-  "https://gurucool-frontend-three.vercel.app"
+  "https://gurucool-frontend.vercel.app"
+
 ];
 
 app.use(
